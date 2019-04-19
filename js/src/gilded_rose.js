f517a06d8b61a488f949c8482c55da389b8e4f84
rouh.alavi <ralavi@virtusa.com>
2019-04-19 15:45:31 +0100

    warm up with add some notes to javascript

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -1,11 +1,30 @@
-function Item(name, sell_in, quality) {
+/**
+ * create an item with initial state
+ * @param {String} name 
+ * @param {Number} sell_in 
+ * @param {Number} quality 
+ * @param {function} stateFunc
+ * @example stateFunction should look like stateFunc = (previousState, events = null ) => (nextState);
+ */
+function Item(name, sell_in, quality, stateFunc) {
   this.name = name;
   this.sell_in = sell_in;
   this.quality = quality;
+  this.stateFunction = stateFunc;
 }
 
 var items = []
 
+/**
+ * This is pretty much unreadable
+ * with couple of down falls:
+ * 1- Hard to maintain/debug
+ * 2- Hard to extend
+ * 3- Almost impossible to read ;)
+ * @todo Convert it to the following structure
+ * @example every product category will have its own state function
+ * @see state-functions,state-machines
+ */
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
     if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
