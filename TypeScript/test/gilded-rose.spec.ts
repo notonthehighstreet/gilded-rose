6253cef1610516aa4af6412ceffdebeea9ba7f27
Luke Barton <luke@lukebarton.co.uk>
2019-05-29 15:35:05 +0100

    Initial test that generic items have sell by date reduced

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -1,12 +1,23 @@
 import { expect } from 'chai';
 import { Item, GildedRose } from '../app/gilded-rose';
 
+function itemWithNamedProperties({name, sellIn, quality}: { name: string, sellIn: number, quality: number}) {
+    return new Item(name, sellIn, quality);
+}
+
 describe('Gilded Rose', function () {
-
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+    describe('for generic items', () => {
+        const itemName = 'generic item';
+        it('should reduce sell by date by 1', function() {
+            const gildedRose = new GildedRose([ 
+                itemWithNamedProperties({
+                    name: itemName,
+                    sellIn: 5,
+                    quality: 0
+                }) 
+            ]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].sellIn).to.equal(4);
+        });
     });
-
 });
