6826b70a974142a1b83b86c777fc724c7506b4c9
Luke Barton <luke@lukebarton.co.uk>
2019-05-29 16:45:21 +0100

    Test for quality reduction of generic items

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -23,5 +23,56 @@ describe('Gilded Rose', function () {
                 })
             });
         });
+        describe('when not expired', () => {
+            const notExpiredSellByDate = 1;
+            describe('should reduce quality by 1, lower bound by 0', function() {
+                [
+                    {in: 0, out: 0},
+                    {in: 1, out: 0},
+                    {in: 2, out: 1},
+                    {in: 3, out: 2},
+                ].forEach((testCase) => {
+                    const originalQuality = testCase.in;
+                    const expectedQuality = testCase.out;
+                    it(`from ${originalQuality} to ${expectedQuality}`, () => {
+                        const gildedRose = new GildedRose([
+                            itemWithNamedProperties({
+                                name: itemName,
+                                sellIn: notExpiredSellByDate,
+                                quality: originalQuality
+                            })
+                        ]);
+                        const items = gildedRose.updateQuality();
+                        expect(items[0].quality).to.equal(expectedQuality);
+                    })
+                });
+            });
+        });
+        describe('when expired', () => {
+            const expiredSellByDate = 0;
+            describe('should reduce quality by 2, lower bound by 0', function() {
+                [
+                    {in: 0, out: 0},
+                    {in: 1, out: 0},
+                    {in: 2, out: 0},
+                    {in: 3, out: 1},
+                    {in: 4, out: 2},
+                ].forEach((testCase) => {
+                    const originalQuality = testCase.in;
+                    const expectedQuality = testCase.out;
+                    it(`from ${originalQuality} to ${expectedQuality}`, () => {
+                        const gildedRose = new GildedRose([
+                            itemWithNamedProperties({
+                                name: itemName,
+                                sellIn: expiredSellByDate,
+                                quality: originalQuality
+                            })
+                        ]);
+                        const items = gildedRose.updateQuality();
+                        expect(items[0].quality).to.equal(expectedQuality);
+                    })
+                });
+            });
+        });
     });
 });
