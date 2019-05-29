5fe901e5a81d6dac071058f2f12e5c370fbc9702
Luke Barton <luke@lukebarton.co.uk>
2019-05-29 16:59:46 +0100

    Test Aged Brie increases quality

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -75,4 +75,73 @@ describe('Gilded Rose', function () {
             });
         });
     });
+    describe('for Aged Brie', () => {
+        const itemName = 'Aged Brie';
+        describe('should reduce sell by date', function() {
+            [-2, -1, 0, 1, 2, 3, 4, 5].forEach((sellByDate) => {
+                it(`from ${sellByDate} to ${sellByDate-1}`, () => {
+                    const gildedRose = new GildedRose([
+                        itemWithNamedProperties({
+                            name: itemName,
+                            sellIn: sellByDate,
+                            quality: 0
+                        })
+                    ]);
+                    const items = gildedRose.updateQuality();
+                    expect(items[0].sellIn).to.equal(sellByDate-1);
+                })
+            });
+        });
+        describe('when not expired', () => {
+            const notExpiredSellByDate = 1;
+            describe('should increase quality by 1, upper bound by 50', function() {
+                [
+                    {in: 0, out: 1},
+                    {in: 1, out: 2},
+                    {in: 2, out: 3},
+                    {in: 50, out: 50},
+                ].forEach((testCase) => {
+                    const originalQuality = testCase.in;
+                    const expectedQuality = testCase.out;
+                    it(`from ${originalQuality} to ${expectedQuality}`, () => {
+                        const gildedRose = new GildedRose([
+                            itemWithNamedProperties({
+                                name: itemName,
+                                sellIn: notExpiredSellByDate,
+                                quality: originalQuality
+                            })
+                        ]);
+                        const items = gildedRose.updateQuality();
+                        expect(items[0].quality).to.equal(expectedQuality);
+                    })
+                });
+            })
+        });
+        describe('when expired', () => {
+            const expiredSellByDate = 0;
+            describe('should increase quality by 2, upper bound by 50', function() {
+                [
+                    {in: 0, out: 2},
+                    {in: 1, out: 3},
+                    {in: 2, out: 4},
+                    {in: 3, out: 5},
+                    {in: 50, out: 50},
+                ].forEach((testCase) => {
+                    const originalQuality = testCase.in;
+                    const expectedQuality = testCase.out;
+                    it(`from ${originalQuality} to ${expectedQuality}`, () => {
+                        const gildedRose = new GildedRose([
+                            itemWithNamedProperties({
+                                name: itemName,
+                                sellIn: expiredSellByDate,
+                                quality: originalQuality
+                            })
+                        ]);
+                        const items = gildedRose.updateQuality();
+                        expect(items[0].quality).to.equal(expectedQuality);
+                    })
+                });
+            });
+        });
+    });
 });
