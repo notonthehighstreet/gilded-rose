82b6b061e6e9f0c32c30595b7a85374efb95e7f2
Luke Barton <luke@lukebarton.co.uk>
2019-05-30 08:44:22 +0100

    Test concert ticket quality changes

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -177,4 +177,130 @@ describe('Gilded Rose', function () {
             });
         });
     });
+    describe('for Backstage passes to a TAFKAL80ETC concert', () => {
+        const itemName = 'Backstage passes to a TAFKAL80ETC concert';
+        describe('should reduce sell by date', function() {
+            [-2, -1, 0, 1, 2, 3, 4, 5].forEach((sellByDate) => {
+                it(`from ${sellByDate} to ${sellByDate-1}`, () => {
+                    const gildedRose = new GildedRose([
+                        itemWithNamedProperties({
+                            name: itemName,
+                            sellIn: sellByDate,
+                            quality: 0
+                        })
+                    ]);
+                    const items = gildedRose.updateQuality();
+                    expect(items[0].sellIn).to.equal(sellByDate-1);
+                })
+            });
+        });
+        describe('when not expired', () => {
+            describe('when more than 10 days remaining', () => {
+                [ 11, 12, 13 ].forEach((sellByDate) => {
+                    describe(`at ${sellByDate} days remaining, should increase quality by 1, upper bound by 50`, function() {
+                        [
+                            {in: 0, out: 1},
+                            {in: 1, out: 2},
+                            {in: 2, out: 3},
+                            {in: 50, out: 50},
+                        ].forEach((testCase) => {
+                            const originalQuality = testCase.in;
+                            const expectedQuality = testCase.out;
+                            it(`from ${originalQuality} to ${expectedQuality}`, () => {
+                                const gildedRose = new GildedRose([
+                                    itemWithNamedProperties({
+                                        name: itemName,
+                                        sellIn: sellByDate,
+                                        quality: originalQuality
+                                    })
+                                ]);
+                                const items = gildedRose.updateQuality();
+                                expect(items[0].quality).to.equal(expectedQuality);
+                            })
+                        });
+                    })
+                })
+            })
+            describe('when 10 or less days remaining, but more than 5', () => {
+                [ 6, 7, 8, 9, 10 ].forEach((sellByDate) => {
+                        describe(`at ${sellByDate} days remaining, should increase quality by 2, upper bound by 50`, function() {
+                        [
+                            {in: 0, out: 2},
+                            {in: 1, out: 3},
+                            {in: 2, out: 4},
+                            {in: 3, out: 5},
+                            {in: 50, out: 50},
+                        ].forEach((testCase) => {
+                            const originalQuality = testCase.in;
+                            const expectedQuality = testCase.out;
+                            it(`from ${originalQuality} to ${expectedQuality}`, () => {
+                                const gildedRose = new GildedRose([
+                                    itemWithNamedProperties({
+                                        name: itemName,
+                                        sellIn: sellByDate,
+                                        quality: originalQuality
+                                    })
+                                ]);
+                                const items = gildedRose.updateQuality();
+                                expect(items[0].quality).to.equal(expectedQuality);
+                            })
+                        });
+                    })
+                })
+            })
+        });
+        describe('when 5 or less days remaining', () => {
+                [ 1, 2, 3, 4, 5 ].forEach((sellByDate) => {
+                    describe(`at ${sellByDate} days remaining, should increase quality by 3, upper bound by 50`, function() {
+                    [
+                        {in: 0, out: 3},
+                        {in: 1, out: 4},
+                        {in: 2, out: 5},
+                        {in: 3, out: 6},
+                        {in: 50, out: 50},
+                    ].forEach((testCase) => {
+                        const originalQuality = testCase.in;
+                        const expectedQuality = testCase.out;
+                        it(`from ${originalQuality} to ${expectedQuality}`, () => {
+                            const gildedRose = new GildedRose([
+                                itemWithNamedProperties({
+                                    name: itemName,
+                                    sellIn: sellByDate,
+                                    quality: originalQuality
+                                })
+                            ]);
+                            const items = gildedRose.updateQuality();
+                            expect(items[0].quality).to.equal(expectedQuality);
+                        })
+                    });
+                })
+            })
+        });
+        describe('when expired', () => {
+            const expiredSellByDate = 0;
+            describe('should immediately reduce quality to 0', function() {
+                [
+                    {in: 0, out: 0},
+                    {in: 1, out: 0},
+                    {in: 2, out: 0},
+                    {in: 3, out: 0},
+                    {in: 50, out: 0},
+                ].forEach((testCase) => {
+                    const originalQuality = testCase.in;
+                    const expectedQuality = testCase.out;
+                    it(`from ${originalQuality} to ${expectedQuality}`, () => {
+                        const gildedRose = new GildedRose([
+                            itemWithNamedProperties({
+                                name: itemName,
+                                sellIn: expiredSellByDate,
+                                quality: originalQuality
+                            })
+                        ]);
+                        const items = gildedRose.updateQuality();
+                        expect(items[0].quality).to.equal(expectedQuality);
+                    })
+                });
+            });
+        });
+    });
 });
