ae4a81193b9a6023c5fac484f78996be2fea0bdc
Tushar Singhal <tsinghal@sapient.com>
2019-07-16 15:40:23 +0100

    created a new method update_quality_refactored

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -53,3 +53,51 @@ function update_quality() {
     }
   }
 }
+
+function update_quality_refactored() {
+  items.forEach(function (item, index, array) {
+    console.log(item.id, item.title);
+    var name = item.name
+    var quality = item.quality
+    var sell_in = item.sell_in
+
+    switch (name) {
+      case "Aged Brie":
+        quality = quality == 50 ? 50 : quality + 1
+        break;
+      case "Backstage passes to a TAFKAL80ETC concert":
+        var increaseCounter = 0
+        if (sell_in < 6 && sell_in > -1) {
+          increaseCounter = 3
+          quality = quality == 50 ? 50 : quality + increaseCounter
+        }
+        else if (sell_in < 11 && sell_in > 5) {
+          increaseCounter = 2
+          quality = quality == 50 ? 50 : quality + increaseCounter
+        }
+        else if (sell_in > 10) {
+          increaseCounter = 1
+          quality = quality == 50 ? 50 : quality + increaseCounter
+        }
+        else {
+          quality = 0
+        }
+        break
+      case "Sulfuras, Hand of Ragnaros":
+        quality = 80
+        break;
+      case "Conjured Mana Cake":
+        quality = sell_in > 0 ? quality - 2 : quality - 4
+        if (quality < 0) quality = 0
+        break
+      default:
+        quality = sell_in > 0 ? quality - 1 : quality - 2
+        if (quality < 0) quality = 0
+        break
+    }
+
+    item.quality = quality
+    item.sell_in = name == "Sulfuras, Hand of Ragnaros" ? sell_in : sell_in - 1
+
+  });
+}
