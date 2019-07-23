ea0f5d5ac555b5cbd0dd4031605414632b578e35
Eriksson, Linda (Associate Software Developer) <linda.eriksson@sky.uk>
2019-07-23 17:38:47 +0100

    Added conditional for conjured items

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -11,7 +11,11 @@ function update_quality() {
     if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (items[i].quality > 0) {
         if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
+          if (items[i].name.includes('Conjured')) {
+            items[i].quality = items[i].quality - 2
+          } else {
+            items[i].quality = items[i].quality - 1
+          }
         }
       }
     } else {
@@ -39,7 +43,11 @@ function update_quality() {
         if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
           if (items[i].quality > 0) {
             if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
+              if (items[i].name.includes('Conjured')) {
+                items[i].quality = items[i].quality - 2
+              } else {
+                items[i].quality = items[i].quality - 1
+              }    
             }
           }
         } else {
