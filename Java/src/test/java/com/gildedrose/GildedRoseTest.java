8005cfdb1fe8c22dadb333e0f072c1d8cc9bca9c
Dylan Green <dylan.green@theculturetrip.com>
2019-08-26 14:56:22 +0100

    Added Google Truth assertion library and fixed existing broken test

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,22 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertThat;
 
 public class GildedRoseTest {
 
+    //NOTE: Outside of a interview test environment, I would delete this test as,
+    //static item order is not enforced in the problem statement
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void testItemPositionUnmodified() {
+        Item item_1 = new Item("item_1", 0, 0);
+        Item item_2 = new Item("item_2", 0, 0);
+        Item item_3 = new Item("item_3", 0, 0);
+        Item[] items = new Item[]{item_1, item_2, item_3};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertThat(item_1.name).isEqualTo(app.items[0].name);
     }
 
 }
