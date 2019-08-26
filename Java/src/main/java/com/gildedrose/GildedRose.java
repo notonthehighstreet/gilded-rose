d805a82452de68031b450daf271c3f76ea6889cf
Dylan Green <dylan.green@theculturetrip.com>
2019-08-26 18:36:50 +0100

    extracted Conjured Items out of the Generic Item logic at GildedRose.updateQuality()

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,62 +9,69 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            switch (item.name) {
-                case "Aged Brie":
+            if (item.name.equals("Aged Brie")) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
                     }
-                    item.sellIn = item.sellIn - 1;
+                }
+            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-                    if (item.sellIn < 0) {
+                    if (item.sellIn < 11) {
                         if (item.quality < 50) {
                             item.quality = item.quality + 1;
                         }
                     }
-                    break;
-                case "Backstage passes to a TAFKAL80ETC concert":
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
 
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
                         }
                     }
-                    item.sellIn = item.sellIn - 1;
+                }
+                item.sellIn = item.sellIn - 1;
 
-                    if (item.sellIn < 0) {
-                        item.quality = 0;
-                    }
-                    break;
-                case "Sulfuras, Hand of Ragnaros":
-                    //Ignore
-                    break;
-                default:
+                if (item.sellIn < 0) {
+                    item.quality = 0;
+                }
+            } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                //Ignore
+
+            } else if (item.name.startsWith("Conjured")) {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
                     if (item.quality > 0) {
                         item.quality = item.quality - 1;
-                        if (item.name.startsWith("Conjured") && item.quality > 0) {
-                            item.quality = item.quality - 1;
-                        }
                     }
-                    item.sellIn = item.sellIn - 1;
+                }
+                item.sellIn = item.sellIn - 1;
 
-                    if (item.sellIn < 0) {
+                if (item.sellIn < 0) {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
                         if (item.quality > 0) {
                             item.quality = item.quality - 1;
-                            if (item.name.startsWith("Conjured") && item.quality > 0) {
-                                item.quality = item.quality - 1;
-                            }
                         }
                     }
-                    break;
+                }
+            } else {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0) {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
+                    }
+                }
             }
         }
     }
