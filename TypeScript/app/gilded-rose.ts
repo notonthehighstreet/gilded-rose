04c5227cd6a283ac5574ff21a8d8d96c2e862110
Paul Breslin <paul.breslin@outlook.com>
2019-10-14 08:37:15 +0100

    Refactors updateQuality method

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -18,61 +18,56 @@ export class GildedRose {
   }
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (
-        this.items[i].name != "Aged Brie" &&
-        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-      ) {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
+    // Iterate through items, update Quality and SellIn values
+    const updatedItems = this.items.map(item => {
+      switch (item.name) {
+        case "Aged Brie": {
+          item.quality += 1;
+          break;
         }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-
-          if (
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (
-            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
+        case "Backstage passes to a TAFKAL80ETC concert": {
+          if (item.sellIn <= 10 && item.sellIn > 5) {
+            item.quality += 2;
+          } else if (item.sellIn <= 5 && item.sellIn > 0) {
+            item.quality += 3;
+          } else if (item.sellIn < 0) {
+            item.quality = 0;
           } else {
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality;
+            item.quality += 1;
           }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+          break;
+        }
+        case "Sulfuras, Hand of Ragnaros": {
+          item.quality = 80;
+          break;
+        }
+        default: {
+          // TODO - Add variation for "Conjured" items
+          if (item.quality > 0) {
+            if (item.sellIn < 0) {
+              // Past sell by date, item decreases twice as fast
+              item.quality -= 2;
+            } else {
+              // Not past sell by date, default quality reduction
+              item.quality -= 1;
+            }
           }
         }
       }
-    }
 
-    return this.items;
+      // Reset quality to max limit if it has gone over
+      if (item.quality > 50) {
+        item.quality = 50;
+      }
+
+      // Reset quality to min limit if it has gone under
+      if (item.quality < 0) {
+        item.quality = 0;
+      }
+
+      item.sellIn -= 1;
+    });
+
+    return updatedItems;
   }
 }
