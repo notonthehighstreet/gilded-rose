5f808ba3a98939ad1a38499c48618467dc24c4cc
Rupali Ghate <ghate.rupali@gmail.com>
2019-12-10 20:39:38 +0000

    Added conditions to update quality of 'Conjured' items. Updated GildedRoseTests.

--- swift/Sources/GildedRose.swift
+++ swift/Sources/GildedRose.swift
@@ -1,3 +1,4 @@
+import Foundation
 
 public class GildedRose {
     var items:[Item]
@@ -12,14 +13,23 @@ public class GildedRose {
             if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
                 if (items[i].quality > 0) {
                     if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
+                        // "Conjured" items degrade in Quality twice as fast as normal items
+                        if(items[i].name.lowercased().contains("conjured")) {
+                            // [Rupali]: This is first way of checking the quantity for 'Conjured Mana Cake'
+                            items[i].quality = items[i].quality > 1 ? items[i].quality - 2 : 0
+                        } else {
+                            items[i].quality = items[i].quality - 1
+                        }
                     }
                 }
             } else {
+                // The Quality of an item is never more than 50
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1
                     
+                    // "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
                     if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+                        // Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1
@@ -35,6 +45,7 @@ public class GildedRose {
                 }
             }
             
+            // "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
             if (items[i].name != "Sulfuras, Hand of Ragnaros") {
                 items[i].sellIn = items[i].sellIn - 1
             }
@@ -42,15 +53,23 @@ public class GildedRose {
             if (items[i].sellIn < 0) {
                 if (items[i].name != "Aged Brie") {
                     if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                        // The Quality of an item is never negative
                         if (items[i].quality > 0) {
                             if (items[i].name != "Sulfuras, Hand of Ragnaros") {
                                 items[i].quality = items[i].quality - 1
+                                // "Conjured" items degrade in Quality twice as fast as normal items
+                                // [Rupali]: This is second way of checking the quantity for 'Conjured Mana Cake'
+                                if (items[i].name.lowercased().contains("conjured") && items[i].quality > 0) {
+                                    items[i].quality = items[i].quality - 1
+                                }
                             }
                         }
                     } else {
+                        // Quality drops to zero after the conccert. backstage passes to concert
                         items[i].quality = items[i].quality - items[i].quality
                     }
                 } else {
+                    // "Aged Brie" actually increases in Quality the older it gets
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1
                     }
--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -4,12 +4,36 @@ import XCTest
 @testable import GildedRose
 
 class GildedRoseTests: XCTestCase {
-
+    
     func testFoo() {
         let items = [Item(name: "foo", sellIn: 0, quality: 0)]
-        let app = GildedRose(items: items);
-        app.updateQuality();
-        XCTAssertEqual("fixme", app.items[0].name);
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual("foo", app.items[0].name)
+    }
+    
+    func testConjuredItemQualityBeforeSellIn() {
+        let days = 1
+        let items = [Item(name: "Conjured Mana Cake", sellIn: days, quality: 6)]
+        let app = GildedRose(items: items)
+        
+        for _ in 0..<days {
+            app.updateQuality()
+        }
+        
+        XCTAssertEqual(4, app.items[0].quality)
+    }
+    
+    func testConjuredItemQualityAfterSellIn() {
+        let days = 3
+        let items = [Item(name: "Conjured Cake1", sellIn: days, quality: 2)]
+        let app = GildedRose(items: items)
+        
+        for _ in 0..<days {
+            app.updateQuality()
+        }
+        
+        XCTAssertEqual(0, app.items[0].quality)
     }
 }
 
