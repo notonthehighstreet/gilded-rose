ce0a7215f8cf3626f4ad24fe36c764bfdcb3cd80
Conor Malone <conor.malone@hostmaker.co>
2020-01-18 21:30:00 +0000

    createItem test helper with defaults

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -1,8 +1,14 @@
 import { expect } from 'chai';
-import { Item, GildedRose } from '../app/gilded-rose';
+import { Item, GildedRose, ItemTypes } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
 
+  const INITIAL_QUALITY = 10;
+  const CURRENT_SELL_IN = 20;
+  const EXPIRED_SELL_IN = -1;
+  function createItem(itemType: ItemTypes, sellIn: number = CURRENT_SELL_IN, quality: number = INITIAL_QUALITY) {
+    return new Item(itemType, sellIn, quality);
+  }
   it('should foo', function () {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
@@ -14,24 +20,22 @@ describe('Gilded Rose', function () {
     describe("Normal Items", () => {
 
       it("should decrease in quality by one", () => {
-        const initialQuality = 10;
-        const normalItem = new Item('normal item', 10, initialQuality);
+        const normalItem = createItem(ItemTypes.NORMAL_ITEM);
         const gildedRose = new GildedRose([normalItem]);
         const items = gildedRose.updateQuality();
-        expect(items[0].quality).to.equal(initialQuality - 1);
+        expect(items[0].quality).to.equal(INITIAL_QUALITY - 1);
       });
 
       it("After sell by date past, quality should degrade twice as fast", () => {
-        const initialQuality = 10;
-        const normalItem = new Item('normal item', -1, initialQuality);
+        const normalItem = createItem(ItemTypes.NORMAL_ITEM, EXPIRED_SELL_IN);
         const gildedRose = new GildedRose([normalItem]);
         const items = gildedRose.updateQuality();
-        expect(items[0].quality).to.equal(initialQuality - 2);
+        expect(items[0].quality).to.equal(INITIAL_QUALITY - 2);
       });
 
       it("quality can never be less than 0", () => {
         const initialQuality = 0;
-        const normalItem = new Item('normal item', "4", initialQuality);
+        const normalItem = createItem(ItemTypes.NORMAL_ITEM, 4, initialQuality);
         const gildedRose = new GildedRose([normalItem]);
         const items = gildedRose.updateQuality();
         expect(items[0].quality).to.equal(0);
