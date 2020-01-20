ad9ff82e8d97b6f7b6eb4bf2f49c0998aa45bd66
Conor Malone <conor.malone@hostmaker.co>
2020-01-20 13:30:14 +0000

    Refactored backstage pass to use constants

--- TypeScript/app/itemUpdaters/BackStagePass.ts
+++ TypeScript/app/itemUpdaters/BackStagePass.ts
@@ -2,14 +2,20 @@ import { inRange } from 'lodash';
 import DefaultItemUpdater from './Default';
 import { Item } from '../gilded-rose';
 
+const DOUBLE_RATE_MAX_RANGE = 11;
+const DOUBLE_RATE_MIN_RANGE = 6;
+
+const TRIPPLE_RATE_MAX_RANGE = 6;
+const TRIPPLE_RATE_MIN_RANGE = 0;
+
 
 export default class BackStagePassItemUpdater extends DefaultItemUpdater {
 
     protected getQualityDifference(item: Item): number {
         const { sellIn } = item;
-        if(inRange(sellIn, 6, 11)) {
+        if(inRange(sellIn, DOUBLE_RATE_MIN_RANGE, DOUBLE_RATE_MAX_RANGE)) {
             return 2;
-        } else if(inRange(sellIn, 0, 6)) {
+        } else if(inRange(sellIn, TRIPPLE_RATE_MIN_RANGE, TRIPPLE_RATE_MAX_RANGE)) {
             return 3
         } else if(sellIn < 0) {
             return -item.quality;
