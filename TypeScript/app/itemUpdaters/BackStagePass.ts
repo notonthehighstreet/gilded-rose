91fb1fc8ffc062e66823e7a78a181802852f4f7a
Conor Malone <conor.malone@hostmaker.co>
2020-01-20 13:36:27 +0000

    Refactored backStagePass to improve readability

--- TypeScript/app/itemUpdaters/BackStagePass.ts
+++ TypeScript/app/itemUpdaters/BackStagePass.ts
@@ -11,13 +11,24 @@ const TRIPPLE_RATE_MIN_RANGE = 0;
 
 export default class BackStagePassItemUpdater extends DefaultItemUpdater {
 
+    private usesDoubleRate(item: Item): boolean {
+        return inRange(item.sellIn, DOUBLE_RATE_MIN_RANGE, DOUBLE_RATE_MAX_RANGE);
+    }
+
+    private usesTrippleRate(item: Item): boolean {
+        return inRange(item.sellIn, TRIPPLE_RATE_MIN_RANGE, TRIPPLE_RATE_MAX_RANGE);
+    }
+
+    private isConcertOver(item: Item): boolean {
+        return item.sellIn < 0;
+    }
+
     protected getQualityDifference(item: Item): number {
-        const { sellIn } = item;
-        if(inRange(sellIn, DOUBLE_RATE_MIN_RANGE, DOUBLE_RATE_MAX_RANGE)) {
+        if(this.usesDoubleRate(item)) {
             return 2;
-        } else if(inRange(sellIn, TRIPPLE_RATE_MIN_RANGE, TRIPPLE_RATE_MAX_RANGE)) {
+        } else if(this.usesTrippleRate(item)) {
             return 3
-        } else if(sellIn < 0) {
+        } else if(this.isConcertOver(item)) {
             return -item.quality;
         } else {
             return 1;
